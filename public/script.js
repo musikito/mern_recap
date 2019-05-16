8da49ffc2533b5715e49ac0fdbfe533254c6264b
jose marte <tumusicalatina@gmail.com>
2019-05-16 15:32:41 -0400

    Get user info

--- public/script.js
+++ public/script.js
@@ -1,1 +1,9 @@
-console.log("working");
+function getUserInfo(){
+   const input = document.getElementById("userName").value;
+
+   //console.log(input);
+   axios.get("/showprofile/" + input)
+   .then(response =>{
+       console.log("using axios", response.data);
+   })
+}// getUserInfo()
